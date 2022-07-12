65dd91df2b7e6c3aed2ebda1b442bde695329f3d
Victoria David <victoria@thedavids.us>
2022-07-12 16:32:38 -0400

    drag and drop features implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,6 +113,55 @@ $(".list-group").on("blur", "input[type='text']", function()
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable
+({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event)
+  {
+    console.log("activate", this);
+  },
+  deactivate: function(event)
+  {
+    console.log("deactivate", this);
+  },
+  over: function(event)
+  {
+    console.log("out", event.target);
+  },
+  out: function(event)
+  {
+    console.log("out", event.target);
+  },
+  update: function(event)
+  {
+    //declare array to store data in
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this).children().each(function()
+    {
+      var text = $(this).find("p").text().trim();
+      var date = $(this).find("span").text().trim();
+
+      //add task data to the temp array as an object
+      tempArr.push(
+      {
+        text: text,
+        date: date
+      });
+    });
+
+    //trim down list's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+  
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function()
 {
@@ -161,6 +210,26 @@ $("#remove-tasks").on("click", function()
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+//droppable trash function
+$("#trash").droppable(
+{
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui)
+  {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui)
+  {
+    console.log("out");
+  },
+  out: function(event, ui)
+  {
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
